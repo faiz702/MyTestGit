13c4c0e6ab50dfc15b0a3a33bb74089ea785d061
Faiz <36890899+faiz702@users.noreply.github.com>
2018-07-08 17:40:54 +0300

    Update app.component.ts

--- MyTestApp/src/app/app.component.ts
+++ MyTestApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AL Zamil Heavy Industries Ltd';
+  title = 'AL Zamil Heavy Industries Ltd...';
 }
